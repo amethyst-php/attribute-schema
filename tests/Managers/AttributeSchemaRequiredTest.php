b187c7b60deec4be5b01e999fe80e44d447d4632
Railken <railken@protonmail.ch>
2020-09-19 03:34:36 +0200

    add required test

---
+++ tests/Managers/AttributeSchemaRequiredTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Amethyst\Tests\Managers;
+
+use Amethyst\Fakers\AttributeSchemaFaker;
+use Amethyst\Fakers\FooFaker;
+use Amethyst\Managers\AttributeSchemaManager;
+use Amethyst\Managers\FooManager;
+use Amethyst\Models\Foo;
+use Amethyst\Tests\BaseTest;
+use Railken\Lem\Support\Testing\TestableBaseTrait;
+use Symfony\Component\Yaml\Yaml;
+
+class AttributeSchemaRequiredTest extends AttributeSchemaCommonTest
+{
+    public function testRequired()
+    {
+        $attribute = (new AttributeSchemaManager())->createOrFail([
+            'name'   => 'field_required',
+            'schema' => 'Number',
+            'model'  => 'foo',
+            'required' => true
+        ])->getResource();
+
+        $fooManager = new FooManager();
+
+        $parameters = FooFaker::make()->parameters();
+        $result = $fooManager->create($parameters);
+
+        $this->assertEquals('FOO_FIELD_REQUIRED_NOT_DEFINED', $result->getSimpleErrors()[0]['code']);
+        
+        $attribute->delete();
+    }
+}
